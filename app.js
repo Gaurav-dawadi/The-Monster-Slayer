583df1c6458163ee2ff8f8280e26fc58cd9fe9a5
Gaurav-dawadi <gaurabdawadi@gamil.com>
2021-02-04 12:22:52 +0545

    feat: add special attack method

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = Vue.createApp({
     data(){
         return{
             monsterHealth : 100,
-            playerHealth : 100
+            playerHealth : 100,
+            round: 0
         }
     },
     computed: {
@@ -19,11 +20,18 @@ const app = Vue.createApp({
     },
     methods: {
         attackMonster(){
-            const attackValue = randomValue(8, 15);
+            const attackValue = randomValue(5, 11);
             this.playerHealth -= attackValue;
         },
         attackPlayer(){
-            const attackValue = randomValue(5, 12);
+            this.round ++;
+            const attackValue = randomValue(4, 10);
+            this.monsterHealth -= attackValue;
+            this.attackMonster();
+        },
+        specialAttack(){
+            this.round ++;
+            const attackValue = randomValue(10, 15);
             this.monsterHealth -= attackValue;
             this.attackMonster();
         }
